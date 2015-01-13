5d23d8a6651c66de03ed4c2f6a0cfdd100df4dde
Desmond Elliott <des.elliott@gmail.com>
2015-01-13 11:47:52 +0100

    Adding a new version of the updateAnnotationLocation script that will correctly help people update the locations of their raw data

--- emnlp2013/updateAnnotationLocations.sh
+++ emnlp2013/updateAnnotationLocations.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-for x in `ls */annotations-train`; do sed -i -r 's/vlt/vlt\/gold/' $x; done;
-for x in `ls */annotations-dev`; do sed -i -r 's/vlt/vlt\/gold/' $x; done;
-for x in `ls */annotations-test`; do sed -i -r 's/vlt/vlt\/gold/' $x; done;
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied, you need to supply a valid path."
+    exit 1
+fi
 
-for x in `ls */images-train`; do sed -i -r 's/vlt/vlt\/gold/' $x; done;
-for x in `ls */images-dev`; do sed -i -r 's/vlt/vlt\/gold/' $x; done;
-for x in `ls */images-test`; do sed -i -r 's/vlt/vlt\/gold/' $x; done;
+if [ -z "$1" ]
+  then
+    echo "Empty argument supplied, you need to supply a valid path"
+    exit 1
+fi
+
+newLocation=$1
+oldLocation="^(.*?)+\/" # Matches everything up to the final / that is currently in the file
+
+for x in `ls */annotations-train`; do sed -i -r "s|$oldLocation|$newLocation\/xmlfiles\/|" $x; done;
+for x in `ls */annotations-dev`; do sed -i -r "s|$oldLocation|$newLocation\/xmlfiles\/|" $x; done;
+for x in `ls */annotations-test`; do sed -i -r "s|$oldLocation|$newLocation\/xmlfiles\/|" $x; done;
+
+for x in `ls */images-train`; do sed -i -r "s|$oldLocation|$newLocation\/images\/|" $x; done;
+for x in `ls */images-dev`; do sed -i -r "s|$oldLocation|$newLocation\/images\/|" $x; done;
+for x in `ls */images-test`; do sed -i -r "s|$oldLocation|$newLocation\/images\/|" $x; done;
